bf0609fdbb00784a34baa91c8f28bb7a8c3b833c
Jam <jiamiao@mayiy.com>
2016-05-26 11:07:03 +0800

    Initial Commit

---
+++ CaptureVideoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CaptureVideoDemo
+//
+//  Created by Jam on 16/5/26.
+//  Copyright © 2016年 Jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
